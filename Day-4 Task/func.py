c951a207809c07e55f2db192dbfbfa64204d8d5a
Rishi Dharedi <dharedirishi.rd@gmail.com>
2021-06-09 12:28:32 +0530

    Tasks for Day-4

---
+++ Day-4 Task/func.py
@@ -0,0 +1,5 @@
+def print_name ():
+    print('Rishi Dharedi')
+
+print_name()
+print_name()
---
+++ Day-4 Task/funcWithReturn.py
@@ -0,0 +1,7 @@
+def full_name():
+    name = 'Rishi'
+    surname = 'Dharedi'    
+    return name, surname
+
+name,surname = full_name()
+print('full name: ',name,' ',surname)
