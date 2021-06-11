528b48cd6a0051ed81418597191d36f95715161c
Rishi Dharedi <dharedirishi.rd@gmail.com>
2021-06-11 14:25:57 +0530

    Day-10 Tasks

---
+++ Day-10 Task/myapp/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Student
+from .models import Employee
+
+# Create your views here.
+def homepageview(request):
+    return render(request,'home.html')
+
+def aboutpageview(request):
+    return render(request,'about.html')
+
+def contactpageview(request):
+    return render(request,'contact.html')   
+
+def myform(request):
+    return render(request,'myform.html') 
+
+def process(request):
+    print("Welcome") 
+    print(request.method)
+    print(request.POST)
+    name  = request.POST['name']
+    email  = request.POST['email']
+    password = request.POST['password']
+    age = int(request.POST['age'])
+    phone = int(request.POST['phone'])
+    dob = request.POST['dob']
+    gender = request.POST['gender']
+    city = request.POST['city']
+    skill = request.POST['skill']
+    sem = request.POST['sem']
+    return render(
+         request,"ans.html",{
+            'myname':name,
+            'myemail':email,
+            'mypassword':password,
+            'myage':age,
+            'myphone':phone,
+            'mydob':dob,
+            'mygender':gender,
+            'mycity':city,
+            'myskill':skill,
+            'mysem':sem,
+        })
+
+
+
+class DataList(ListView):
+    model = Student
+    template_name = 'dataList.html'
